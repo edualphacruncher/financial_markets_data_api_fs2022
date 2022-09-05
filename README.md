ac2fdd6b29cad5410c1a47ae71e9ba165375169d
pibence <pipis.bence@gmail.com>
2022-09-05 15:24:22 +0000

    descriptive changes, allowing dates to be specified as strings

---
+++ README.md
@@ -0,0 +1,4 @@
+# Data query API for Financial Markets course at MIT Sloan
+
+This package provides tools for straightforward data loading from specified universes.
+With the get function the user can specify the tickers, universe and date range and will get a pandas dataframe as a result.
--- nvdata.py
+++ nvdata.py
@@ -1,16 +1,17 @@
 import pandas as pd
-from loading_functions.helpers import parse_yaml
-from loading_functions.load_equities import load_equity
-import datetime
+from load.helpers import parse_yaml
+from load.load_equities import load_equity
+import datetime as dt
 
 config_dict = parse_yaml('config.yml')
 
-def get(ticker:str or list, universe:str, date_start:datetime.date, date_end:datetime.date, **kwargs):
+def get(ticker:str or list, universe:str, date_start:str or datetime.date, date_end:str or datetime.date, **kwargs):
     """
     The function returns the desired data for the given ticker, from a given universe.
     The ticker can be given as a string, or a list of strings. If invalid ticker
     is entered, the function will return and empty dataframe. The start and end
-    dates should be specified as datetime.date objects.
+    dates should be specified as datetime.date objects or in a string format
+    like '2020-01-01'.
     
     List of available universe and returned data formats:
         * equities: pandas dataframe with the following columns:
@@ -25,13 +26,27 @@ def get(ticker:str or list, universe:str, date_start:datetime.date, date_end:dat
     if isinstance(ticker, str) or isinstance(ticker, list):
         try:
             universe_route = config_dict[universe]
-        except Exception:
+        except KeyError:
             print('Please use existing universe!')
-
-        if isinstance(date_start, datetime.date) and isinstance(date_end, datetime.date):
-            ret_df = load_equity(ticker, universe_route, date_start, date_end)
-            return ret_df
-        else:
-            print('Please use datetime.date format!')
+            return None
     else:
         print('Please enter the ticker(s) as a string or list of strings!')
+        return None
+
+    if isinstance(date_start, str):
+        try:
+            date_start = dt.datetime.strptime(date_start,'%Y-%m-%d').date()
+        except ValueError:
+            print('Please use datetime.date or the YYYY-MM-DD format for dates')
+            return None
+
+    if isinstance(date_end, str):
+        try:
+            date_end = dt.datetime.strptime(date_end,'%Y-%m-%d').date()
+        except ValueError:
+            print('Please use datetime.date or the YYYY-MM-DD format for dates')
+            return None
+
+    if isinstance(date_start, dt.date) and isinstance(date_end, dt.date):
+            ret_df = load_equity(ticker, universe_route, date_start, date_end)
+            return ret_df
